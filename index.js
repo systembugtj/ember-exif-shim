eb62eee5538337e666ac333a674fbce285f46521
Albert <albert.li@salesforce.com>
2016-09-21 11:20:24 -0700

    add shim for exif-js

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-exif-shim'
+  name: 'ember-exif-shim',
+  included(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/exif-js/exif.js');
+    app.import('vendor/exif.js', {
+      exports: {
+        EXIF: ['default']
+      }
+    });
+  }
 };
