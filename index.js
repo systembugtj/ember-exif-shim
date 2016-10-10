175562dcf3eac1eeaae250d86c87f45ff2e667fd
Albert <albert.li@salesforce.com>
2016-10-10 10:20:37 -0700

    add blueprint path

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
   name: 'ember-exif-shim',
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  },
+
   included(app) {
     this._super.included(app);
     app.import(app.bowerDirectory + '/exif-js/exif.js');
