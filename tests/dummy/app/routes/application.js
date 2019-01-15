ce71ecac01f223f66d2066fb1ca4681c3d652609
Albert Li <albert.li@salesforce.com>
2019-01-15 10:43:27 -0800

    fix error

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route'
 import queryExifTags from "../utils/exif-tag";
 
-export default Ember.Route.extend({
+export default Route.extend({
   model() {
     return queryExifTags("https://raw.githubusercontent.com/exif-js/exif-js/master/example/dsc_09827.jpg").then(tags => {
       return JSON.stringify(tags, null, "\t");
