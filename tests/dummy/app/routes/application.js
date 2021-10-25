01bee9a6785a96b4d64c77e8819f0877b96916f8
Albert Li <albert.li@salesforce.com>
2021-10-25 12:16:15 -0700

    fix: enable husky

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,12 +1,12 @@
 import Route from '@ember/routing/route';
 import queryExifTags from '../utils/exif-tag';
 
-export default Route.extend({
+export default class ApplicationRoute extends Route {
   model() {
     return queryExifTags(
       'https://raw.githubusercontent.com/exif-js/exif-js/master/example/dsc_09827.jpg'
     ).then((tags) => {
       return tags;
     });
-  },
-});
+  }
+}
